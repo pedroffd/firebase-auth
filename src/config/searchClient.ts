6e1bb88d39dea4c12b445bcd09056f081b52f73d
pedroffd <pedro.alcarin@gmail.com>
2022-07-16 03:42:58 -0300

    unused services files removed

--- src/config/searchClient.ts
+++
@@ -1,7 +0,0 @@
-import algoliasearch from "algoliasearch/lite";
-
-const algoliaAppId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string;
-const algoliaSearchApiKey = process.env
-  .NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY as string;
-
-export const searchClient = algoliasearch(algoliaAppId, algoliaSearchApiKey);
